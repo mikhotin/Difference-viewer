b1a6b7e3ff6ce4f8dd5e4ef9a7d9599342b5fed8
alexander <ad.mihotin@gmail.com>
2020-02-14 00:12:36 +0300

    made correct result

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import commander from 'commander';
 import _ from 'lodash';
 
-const genDiff = (file1, file2) => {
+const fs = require('fs');
+
+const parse = (file, func) => func(file);
+
+const genDiff = (pathToFile1, pathToFile2) => {
+  const file1 = parse(pathToFile1, JSON.parse);
+  const file2 = parse(pathToFile2, JSON.parse);
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
   const uniqColl = _.uniq([...keys1, ...keys2]);
@@ -31,6 +37,9 @@ const genDiff = (file1, file2) => {
 }`;
 };
 
+const pathToFile1 = fs.readFileSync('./before.json', 'utf8');
+const pathToFile2 = fs.readFileSync('./after.json', 'utf8');
+
 const makeProgram = () => {
   const program = new commander.Command();
   program
@@ -40,9 +49,7 @@ const makeProgram = () => {
     .option('-f --format [type]', 'output format');
 
   program
-    .action(() => {
-      genDiff();
-    });
+    .action(() => console.log(genDiff(pathToFile1, pathToFile2)));
 
   program.parse(process.argv);
 };
