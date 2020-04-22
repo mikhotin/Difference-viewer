30fca5da43cd4b4c9e4d7697d433a4e341db4550
alexander <ad.mihotin@gmail.com>
2020-04-22 22:46:39 +0300

    created fixture

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,42 +1,8 @@
 import path from 'path';
 import genDiff from '../src/gendiff';
+import data from '../__fixtures__/data';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const data = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      + setting3: {
-            key: value
-        }
-      - setting3: true
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      + baz: bars
-      - baz: bas
-        foo: bar
-      + nest: str
-      - nest: {
-            key: value
-        }
-    }
-  - group2: {
-        abc: 12345
-    }
-  + group3: {
-        fee: 100500
-    }
-}`;
 
 test('diff json', () => {
   expect(genDiff(getFixturePath('before.json'), getFixturePath('after.json'))).toEqual(data);
