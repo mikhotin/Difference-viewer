bce4e920d919b7eecc6786489503dd93fa69ca6c
alexander <ad.mihotin@gmail.com>
2020-05-01 22:20:00 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
-import parse from './parsers/parsers';
+import parse from './parsers';
 import format from './formatters';
 
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filename) => fs.readFileSync(getFullPath(filename), 'utf-8');
-const hasKeyBoth = (obj1, obj2, key) => _.has(obj1, key) && _.has(obj2, key);
-const hasEqualsValue = (obj1, obj2, key) => obj1[key] === obj2[key];
 const isValueObject = (obj1, obj2, key) => _.isObject(obj1[key]) && _.isObject(obj2[key]);
 const createAst = (obj1, obj2) => {
-  const keys = _.union([..._.keys(obj1), ..._.keys(obj2)]).sort();
+  const keys = _.union(_.keys(obj1), _.keys(obj2)).sort();
   const iter = (key) => {
-    if (hasKeyBoth(obj1, obj2, key)) {
-      if (isValueObject(obj1, obj2, key)) {
-        return { type: 'unchanged', key, children: createAst(obj1[key], obj2[key]) };
-      }
-      if (hasEqualsValue(obj1, obj2, key)) {
-        return { type: 'unchanged', key, value: obj1[key] };
-      }
-      return {
-        type: 'changed', key, value: obj2[key], beforeValue: obj1[key],
-      };
+    if (obj1[key] === obj2[key]) {
+      return { type: 'unchanged', key, value: obj1[key] };
     }
 
     if ((_.has(obj1, key) && !_.has(obj2, key))) {
       return { type: 'deleted', key, value: obj1[key] };
     }
 
-    return { type: 'added', key, value: obj2[key] };
+    if ((!_.has(obj1, key) && _.has(obj2, key))) {
+      return { type: 'added', key, value: obj2[key] };
+    }
+
+    if (isValueObject(obj1, obj2, key)) {
+      return { type: 'unchanged', key, children: createAst(obj1[key], obj2[key]) };
+    }
+
+    return {
+      type: 'changed', key, value: obj2[key], beforeValue: obj1[key],
+    };
   };
 
   const ast = keys.map((key) => iter(key));
-
   return ast;
 };
 
