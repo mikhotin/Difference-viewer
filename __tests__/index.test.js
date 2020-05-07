5fa8b5ba74b52485f37351a087583836c9311607
alexander <ad.mihotin@gmail.com>
2020-05-08 00:44:35 +0300

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,15 +5,45 @@ import genDiff from '../src/index';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const fileFormats = ['json', 'yaml', 'ini'];
+let nestedOutput;
+let plainOutput;
+let jsonOutput;
 
-describe.each`
- output        | expected
- ${'nested'}   | ${readFile('nestedOutput.txt')}
- ${'plain'}    | ${readFile('plainOutput.txt')}
- ${'json'}     | ${readFile('jsonOutput.txt')}
-`('$output output', ({ output, expected }) => {
+beforeEach(() => {
+  nestedOutput = readFile('nestedOutput.txt');
+  plainOutput = readFile('plainOutput.txt');
+  jsonOutput = readFile('jsonOutput.txt');
+});
+
+describe('Nested Output', () => {
   test.each(fileFormats)(
     '%s',
-    (format) => expect(genDiff(getFixturePath(`before.${format}`), getFixturePath(`after.${format}`), output)).toEqual(expected),
+    (format) => {
+      const filepath1 = getFixturePath(`before.${format}`);
+      const filepath2 = getFixturePath(`after.${format}`);
+      expect(genDiff(filepath1, filepath2, 'nested')).toEqual(nestedOutput);
+    },
+  );
+});
+
+describe('Plain Output', () => {
+  test.each(fileFormats)(
+    '%s',
+    (format) => {
+      const filepath1 = getFixturePath(`before.${format}`);
+      const filepath2 = getFixturePath(`after.${format}`);
+      expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainOutput);
+    },
+  );
+});
+
+describe('Json Output', () => {
+  test.each(fileFormats)(
+    '%s',
+    (format) => {
+      const filepath1 = getFixturePath(`before.${format}`);
+      const filepath2 = getFixturePath(`after.${format}`);
+      expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonOutput);
+    },
   );
 });
