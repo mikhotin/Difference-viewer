66ce4bde345f7ffcff449f55605c2c4bb809bc3b
mikhotin <ad.mihotin@gmail.com>
2020-05-28 09:58:53 +0300

    refactoring

--- src/formatters/nestedOutput.js
+++ src/formatters/nestedOutput.js
@@ -2,10 +2,10 @@ import _ from 'lodash';
 
 const makeIndent = (depth, indentType) => {
   switch (indentType) {
-    case 2:
-      return ' '.repeat((depth * 4) - indentType);
-    case 4:
-      return ' '.repeat(depth * indentType);
+    case 'firstTypeIndent':
+      return ' '.repeat(depth * 4);
+    case 'secondTypeIndent':
+      return ' '.repeat((depth * 4) - 2);
     default:
       throw new Error(`Unknown indentType: ${indentType}!`);
   }
@@ -16,7 +16,7 @@ const stringify = (val, depth) => {
     return val;
   }
   const key = _.keys(val);
-  return `{\n${makeIndent(depth + 1, 4)}${key}: ${val[key]}\n${makeIndent(depth, 4)}}`;
+  return `{\n${makeIndent(depth + 1, 'firstTypeIndent')}${key}: ${val[key]}\n${makeIndent(depth, 'firstTypeIndent')}}`;
 };
 
 const makeNestedOutput = (ast, depth = 0) => {
@@ -27,22 +27,22 @@ const makeNestedOutput = (ast, depth = 0) => {
 
     switch (type) {
       case 'ast':
-        return `${makeIndent(currentDepth, 4)}${key}: ${makeNestedOutput(children, currentDepth)}`;
+        return `${makeIndent(currentDepth, 'firstTypeIndent')}${key}: ${makeNestedOutput(children, currentDepth)}`;
       case 'unchanged':
-        return `${makeIndent(currentDepth, 4)}${key}: ${(stringify(value, currentDepth))}`;
+        return `${makeIndent(currentDepth, 'firstTypeIndent')}${key}: ${(stringify(value, currentDepth))}`;
       case 'changed':
-        return [`${makeIndent(currentDepth, 2)}+ ${key}: ${stringify(valueAfter, currentDepth)}`,
-          `${makeIndent(currentDepth, 2)}- ${key}: ${stringify(valueBefore, currentDepth)}`].join('\n');
+        return [`${makeIndent(currentDepth, 'secondTypeIndent')}+ ${key}: ${stringify(valueAfter, currentDepth)}`,
+          `${makeIndent(currentDepth, 'secondTypeIndent')}- ${key}: ${stringify(valueBefore, currentDepth)}`].join('\n');
       case 'added':
-        return `${makeIndent(currentDepth, 2)}+ ${key}: ${stringify(value, currentDepth)}`;
+        return `${makeIndent(currentDepth, 'secondTypeIndent')}+ ${key}: ${stringify(value, currentDepth)}`;
       case 'deleted':
-        return `${makeIndent(currentDepth, 2)}- ${key}: ${stringify(value, currentDepth)}`;
+        return `${makeIndent(currentDepth, 'secondTypeIndent')}- ${key}: ${stringify(value, currentDepth)}`;
       default:
         throw new Error(`Unknown type: ${type}!`);
     }
   };
   const result = ast.map((item) => iter(item, depth + 1)).join('\n');
-  return `{\n${result}\n${makeIndent(depth, 4)}}`;
+  return `{\n${result}\n${makeIndent(depth, 'firstTypeIndent')}}`;
 };
 
 export default makeNestedOutput;
